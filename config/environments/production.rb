0fc9aa5172b8ec67982d9d2636227055a0ca27fc
mwafrika <mwafrikajosue@gmail.com>
2022-12-28 12:29:17 +0200

    Fix deployment link

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "episode25sample-production.up.railway.app"
+  config.hosts << "railwaydeployment-production.up.railway.app"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
